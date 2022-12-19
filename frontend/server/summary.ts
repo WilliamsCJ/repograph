581b399c55eb66634230e2b50339d4c19255f8fc
CJ Williams <cj@cjwilliams.io>
2022-12-19 12:31:43 +0000

    Fix URL

--- frontend/server/summary.ts
+++ frontend/server/summary.ts
@@ -1,6 +1,6 @@
 import { GraphSummary } from "../types/components/home/summary";
 
 export async function getSummary(): Promise<GraphSummary> {
-  const res = await fetch(process.env.BACKEND_URL+ "/graph/summary")
+  const res = await fetch(process.env.NEXT_PUBLIC_BACKEND_URL+ "/graph/summary")
   return await res.json() as GraphSummary
 }
