83ef05effc9cdaf5f3b7babc5d9e380ff1bf10ef
CJ Williams <cj@cjwilliams.io>
2023-01-17 22:26:55 +0000

    Demo ready

--- demo/clone.sh
+++ demo/clone.sh
@@ -1,8 +1,6 @@
 git clone https://github.com/tiangolo/fastapi.git
 git clone https://github.com/RDFLib/pyLODE.git
 git clone https://github.com/OmkarPathak/pygorithm.git
-
-inspect4py -i pyLODE -o pyLODE-out -md -rm -si -ld -sc -r
-inspect4py -i fastapi -o fastapi-out -md -rm -si -ld -sc -r
-inspect4py -i pygorithm -o pygorithm-out -md -rm -si -ld -sc -r
+git clone https://github.com/PyCQA/flake8.git
+git clone https://github.com/encode/starlette
 
