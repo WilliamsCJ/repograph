2ea43f68f9f797c56d39351fa5ee48de470de9b7
CJ Williams <cj@cjwilliams.io>
2023-01-20 13:45:31 +0000

    Merge

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,12 +3,8 @@ const withTwin = require('./withTwin.js')
 /** @type {import('next').NextConfig} */
 module.exports = withTwin({
   reactStrictMode: true,
-<<<<<<< Updated upstream
-  output: 'standalone'
-=======
   output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
-  }
->>>>>>> Stashed changes
-});
+  },
+})
