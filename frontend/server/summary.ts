f94f370d4694c6dbbf7735f0b7b31f6611c6518f
CJ Williams <cj@cjwilliams.io>
2023-01-31 17:28:27 +0000

    Fix frontend API slug

--- frontend/server/summary.ts
+++ frontend/server/summary.ts
@@ -2,7 +2,7 @@ import { GraphSummary } from "../types/components/home/summary";
 
 export async function getSummary(): Promise<GraphSummary> {
   const res = await fetch(
-    process.env.NEXT_PUBLIC_BACKEND_URL + "/v1/graph/summary"
+    process.env.NEXT_PUBLIC_BACKEND_URL + "/ggraph/summary"
   );
   return (await res.json()) as GraphSummary;
 }
