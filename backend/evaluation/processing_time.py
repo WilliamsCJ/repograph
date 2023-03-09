0004cd3da0fda85d01b48f6ad028624a413c5215
CJ Williams <cj@cjwilliams.io>
2023-03-09 14:10:38 +0000

    Reorganising imports

--- backend/evaluation/processing_time.py
+++ backend/evaluation/processing_time.py
@@ -1,21 +1,42 @@
-import os
-
-from yaml import UnsafeLoader
-from repograph.container import ApplicationContainer
-import time
+# pragma: no cover
+"""
+Evaluate the performance of RepoGraph
+"""
+# base imports
 import csv
+import logging
+import os
+import shutil
+import time
+
+# pip imports
+from dependency_injector.wiring import inject, Provide
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn
+from tqdm.auto import tqdm
+from yaml import UnsafeLoader
+
+# Entity imports
+from repograph.container import ApplicationContainer
 from repograph.entities.build.service import BuildService
 from repograph.entities.graph.service import GraphService
-from dependency_injector.wiring import inject, Provide
-from tqdm.auto import tqdm
+
+# Utils
 from repograph.utils.logging import configure_logging
-import logging
+
+paths = [
+    "./demo/pygorithm",
+    "./demo/pyLODE",
+    "./demo/flake8",
+    "./demo/fastapi",
+]
 
 repositories = [
-    ("./evaluation/demo/pygorithm", "pygorithm"),
-    ("./evaluation/demo/pyLODE", "pyLODE"),
-    ("./evaluation/demo/flake8", "flake8"),
-    ("./evaluation/demo/fastapi", "FastAPI"),
+    ("pygorithm", "pygorithm"),
+    ("pyLODE", "pylode"),
+    ("flake8", "flake8"),
+    ("fastapi", "fastapi"),
 ]
 
 configure_logging(logging.CRITICAL)
@@ -31,25 +52,48 @@ def collect(
             csvfile, delimiter=",", quotechar='"', quoting=csv.QUOTE_MINIMAL
         )
         writer.writerow(["Repository", "Time (s)", "Nodes", "Relationships"])
-        for i in tqdm(range(5)):
+        for i in tqdm(range(3)):
             for path, name in tqdm(repositories):
-                start = time.time()
-                build.build(
-                    [path],
-                    f"{name}{i}",
-                    "",
-                    prune=True,
-                )
-                execution_time = time.time() - start
-                summary = graph.get_summary(f"{name}{i}")
-                writer.writerow(
-                    [
-                        name,
-                        execution_time,
-                        summary.nodes_total,
-                        summary.relationships_total,
-                    ]
-                )
+                shutil.copytree(f"../demo/{path}", f"./evaluation/{path}")
+                path = f"./evaluation/{path}"
+
+                try:
+                    start = time.time()
+                    build.build(
+                        [path],
+                        f"{name}{i}",
+                        "",
+                        prune=True,
+                    )
+                    execution_time = time.time() - start
+                    summary = graph.get_summary(f"{name}{i}")
+                    writer.writerow(
+                        [
+                            name,
+                            execution_time,
+                            summary.nodes_total,
+                            summary.relationships_total,
+                        ]
+                    )
+                finally:
+                    shutil.rmtree(path)
+
+
+def plot():
+    df = pd.read_csv("./evaluation/processing_time.csv")
+    print(df)
+
+    ax1 = seaborn.scatterplot(x="Nodes", y="Time (s)", data=df, hue="Repository").set(
+        title="Nodes Created vs Processing Time"
+    )
+    fig1 = ax1.get_figure()
+    fig1.savefig("./evaluation/processing_time_vs_nodes.png")
+    plt.clf()
+    ax2 = seaborn.scatterplot(
+        x="Relationships", y="Time (s)", data=df, hue="Repository"
+    ).set(title="Relationships Created vs Processing Time")
+    fig2 = ax2.get_figure()
+    fig2.savefig("./evaluation/processing_time_vs_relationships.png")
 
 
 if __name__ == "__main__":
@@ -59,9 +103,12 @@ if __name__ == "__main__":
 
     open("./evaluation/test.db", "w")
 
+    print("Evaluating processing times...")
+
     try:
         collect()
-    except:
-        pass
+        plot()
+    except Exception as e:
+        print("ERROR: ", e)
     finally:
         os.remove("./evaluation/test.db")
