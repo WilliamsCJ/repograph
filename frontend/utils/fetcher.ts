a793a3d39e017c34ad2cb6acc976cf91be638794
CJ Williams <cj@cjwilliams.io>
2023-03-12 22:14:03 +0000

    Fix fetcher

--- frontend/utils/fetcher.ts
+++ frontend/utils/fetcher.ts
@@ -1,8 +1,6 @@
 const fetcher = (url_path: string) =>
   fetch(
-    `http://${
-      process.env.NODE_ENV == "development" ? "localhost" : "repograph-backend"
-    }:3000${url_path}`
+    `http://localhost:3000${url_path}`
   ).then((res) => res.json());
 
 export default fetcher;
